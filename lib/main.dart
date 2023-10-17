a89857415df885056626c599576becc2b7472371
Dongjin <dongjin@plask.ai>
2023-10-17 23:21:57 +0900

    style, function for a button

--- lib/main.dart
+++ lib/main.dart
@@ -13,69 +13,34 @@ class MyApp extends StatelessWidget {
     await player.load('note$noteNumber.wav');
   }
 
+  Expanded buildKey(int noteNumber, Color color) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () {
+          playSound(noteNumber);
+        },
+        style: ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
+        child: const Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            TextButton(
-              onPressed: () {
-                playSound(1);
-              },
-              child: null,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red)),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                playSound(2);
-              },
-              child: null,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.orange)),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                playSound(3);
-              },
-              child: null,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.yellow)),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                playSound(4);
-              },
-              child: null,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.green)),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                playSound(5);
-              },
-              child: null,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                playSound(6);
-              },
-              child: null,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.indigo)),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                playSound(7);
-              },
-              child: null,
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.purple)),
-            ),
+            buildKey(1, Colors.red),
+            buildKey(2, Colors.orange),
+            buildKey(3, Colors.yellow),
+            buildKey(4, Colors.green),
+            buildKey(5, Colors.blue),
+            buildKey(6, Colors.indigo),
+            buildKey(7, Colors.purple),
           ],
         ),
       ),
